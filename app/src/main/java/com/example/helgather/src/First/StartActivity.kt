baa3df06c7129b9cb9945c8fba500607bd6546ab
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-05-02 18:02:11 +0900

    #8 feat : 첫 앱 사용시 시작화면 설정
    시작 화면시 보여주는 스크롤을 통해 앱에 대한 간략한 설명 및
    AndroidManifest.xml에 activity_start.xml설정

---
+++ app/src/main/java/com/example/helgather/src/First/StartActivity.kt
@@ -0,0 +1,15 @@
+package com.example.helgather.src.First
+
+import android.os.Bundle
+import com.example.helgather.config.BaseActivity
+import com.example.helgather.databinding.ActivityStartBinding
+
+class StartActivity : BaseActivity<ActivityStartBinding>(ActivityStartBinding::inflate) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+}
