639322fd406b3c8980e58a6a158e09d66bdb17fe
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-05-09 00:01:31 +0900

    #15 refactor : 바텀 네비게이션의 전환할때 중복되는 코드들 제거
    모든것이 replace, commitAllowingStateLoss를 사용하기 때문에 이에 대해서 when절로 다 묶은 후 후에 한번에 처리하는것으로 진행

--- app/src/main/java/com/example/helgather/src/Main/MainActivity.kt
+++ app/src/main/java/com/example/helgather/src/Main/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.helgather.src.Main
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.helgather.R
 import com.example.helgather.config.BaseActivity
 import com.example.helgather.databinding.ActivityMainBinding
@@ -13,34 +14,22 @@ class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::infl
         runBottomNavi()
     }
 
-    private fun runBottomNavi(){
+    private fun runBottomNavi() {
         binding.btmNavMain.run {
             setOnItemSelectedListener { item ->
-                when (item.itemId) {
-                    R.id.btm_home -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.frm_main, HomeFragment())
-                            .commitAllowingStateLoss()
-                    }
-
-                    R.id.btm_chatting -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.frm_main, ChattingFragment())
-                            .commitAllowingStateLoss()
-                    }
-
-                    R.id.btm_post -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.frm_main, PostFragment())
-                            .commitAllowingStateLoss()
-                    }
-
-                    R.id.btm_profile -> {
-                        supportFragmentManager.beginTransaction()
-                            .replace(R.id.frm_main, ProfileFragment())
-                            .commitAllowingStateLoss()
-                    }
+                val selectedFragment: Fragment = when (item.itemId) {
+                    R.id.btm_home -> HomeFragment()
+                    R.id.btm_chatting -> ChattingFragment()
+                    R.id.btm_post -> PostFragment()
+                    R.id.btm_profile -> ProfileFragment()
+                    else -> HomeFragment()
                 }
+
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.frm_main, selectedFragment)
+                    .addToBackStack(null) // 백스택에 현재 프래그먼트를 추가
+                    .commitAllowingStateLoss()
+
                 true
             }
             selectedItemId = R.id.btm_home
