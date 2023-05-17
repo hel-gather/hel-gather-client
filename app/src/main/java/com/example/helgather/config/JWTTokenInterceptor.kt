ba84a8f8ecd6087998c1503c38366c73e6f8f317
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-05-17 13:14:02 +0900

    [#25](https://github.com/hel-gather/hel-gather-client/commit/25) feat : 서버와의 기본적인 통신을 위해 BaseResponse, JWTTokenInterceptor
    서버와의 통신을 원활하기 위해 중복적인 BaseResponse에 대해서 템플릿 메서드 디자인 패턴을 적용
    또한 JWTTokenInterceptor도 ApplicationClass딴에서 처음 생성될때 일관적인 sharedpreference와 jwttoken을 사용하기 위해 따로 설정
    retrofit통신을 할때 5초 이상시 타임아웃으로 지정 및 utf-8에 대해 처리를 위해 scalarfactory사용

---
+++ app/src/main/java/com/example/helgather/config/JWTTokenInterceptor.kt
@@ -0,0 +1,20 @@
+package com.example.helgather.config
+
+import com.example.helgather.config.ApplicationClass.Companion.JWT_Token
+import com.example.helgather.config.ApplicationClass.Companion.sSharedPreferences
+import okhttp3.Interceptor
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
+
+class JWTTokenInterceptor : Interceptor {
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val builder: Request.Builder = chain.request().newBuilder()
+        val jwtToken: String? = sSharedPreferences.getString(JWT_Token, null)
+        if (jwtToken != null) {
+            builder.addHeader("X-ACCESS-TOKEN", jwtToken)
+        }
+        return chain.proceed(builder.build())
+    }
+}
