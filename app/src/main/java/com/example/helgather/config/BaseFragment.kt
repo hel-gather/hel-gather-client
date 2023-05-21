e1db9ed06de1016b42a640e8594779edb4c92651
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-05-21 19:50:48 +0900

    #30 feat : toast 메시지 설정
    앞으로의 통신을 할때 문제에 대해서 fragment에서  toastmessage를 보여주기 위해 설정

--- app/src/main/java/com/example/helgather/config/BaseFragment.kt
+++ app/src/main/java/com/example/helgather/config/BaseFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
@@ -28,5 +29,9 @@ abstract class BaseFragment<B : ViewBinding>(
     override fun onDestroyView() {
         _binding = null
         super.onDestroyView()
+    }
+
+    fun showToastMessage(message : String){
+        Toast.makeText(activity,message,Toast.LENGTH_SHORT).show()
     }
 }
