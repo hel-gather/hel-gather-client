3aa35bcf29aa475067182b8075add48c2c2d264e
BENDENG1 <76191161+BENDENG1@users.noreply.github.com>
2023-05-29 06:00:53 +0900

    #60 feat : 프로필 입력에 대한 화면과 서버 통신 모델 추가
    benchpress값에 대해서 통신을 받을때 null이 들어오는데 향후 서버가 처리 요청 대기
    모델에 대해서 수정 완료

---
+++ app/src/main/java/com/example/helgather/src/Main/profile/model/PatchProfileIntroductionRequest.kt
@@ -0,0 +1,8 @@
+package com.example.helgather.src.Main.profile.model
+
+data class PatchProfileIntroductionRequest(
+    val squat: Int?,
+    val benchPress: Int?,
+    val deadlift: Int?,
+    val introduction: String?
+)
---
+++ app/src/main/java/com/example/helgather/src/Main/profile/model/PatchProfileIntroductionResult.kt
@@ -0,0 +1,11 @@
+package com.example.helgather.src.Main.profile.model
+
+data class PatchProfileIntroductionResult(
+    val benchPress: Int,
+    val deadlift: Int,
+    val exerciseCount: Int,
+    val imageUrl: String,
+    val introduction: String,
+    val memberId: Int,
+    val squat: Int
+)
